535f46c9cc57393cbff6a4c1b2c0818e79db05be
Gena <gena.anzin@ukr.net>
2025-08-20 17:38:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name,
+            health=100
+    ):
+        self.health = health
+        self.name = name
+        self.__hidden = False
+
+        if self.health > 0:
+            Animal.alive.append(self)
+
+    def get_hidden(self):
+        return self.__hidden
+
+    def set_hidden(self, value):
+        self.__hidden = value
+
+    def __repr__(self):
+        return f"{{'Name': {self.name}, 'Health': {self.health}, 'Hidden': {self.get_hidden()}}}"
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        self.set_hidden(not self.get_hidden())
+
+
+class Carnivore(Animal):
+
+    def bite(
+            self,
+            target
+    ):
+        if not isinstance(target, Herbivore) or target.get_hidden():
+            return
+
+        target.health -= 50
+        print(f"{self.name} bites {target.name}. {target.name}'s health is now {target.health}.")
+
+        if target.health <= 0:
+            Animal.alive.remove(target)
+            print(f"{target.name} has died.")
+
+
+
+
 # class Animal:
 #     alive = []
 #     def __init__(self, name: str, hidden=False, health: int = 100) -> None:
@@ -22,59 +72,3 @@
 #                 print(f"{animal.name} has died.")
 #         else:
 #             print(f"{self.name} cannot bite {animal.name}. Either they are not a herbivore or they are hidden.")
-
-
-from
-__future__
-import annotations
-from typing import List
-
-
-class Animal:
-    alive: List[Animal] = []
-
-    def __init__(
-            self,
-            name: str,
-            health: int = 100
-    ) -> None:
-        self.health: int = health
-        self.name: str = name
-        self.__hidden: bool = False
-
-        if self.health > 0:
-            Animal.alive.append(self)
-
-    @property
-    def hidden(self) -> bool:
-        return self.__hidden
-
-    @hidden.setter
-    def hidden(self, value: bool) -> None:
-        self.__hidden = value
-
-    def __repr__(self) -> str:
-        return f"{{'Name': {self.name}, 'Health': {self.health}, 'Hidden': {self.hidden}}}"
-
-
-class Herbivore(Animal):
-
-    def hide(self) -> None:
-        self.hidden = not self.hidden
-
-
-class Carnivore(Animal):
-
-    def bite(
-            self,
-            target: Animal
-    ) -> None:
-        if not isinstance(target, Herbivore) or target.hidden:
-            return
-
-        target.health -= 50
-        print(f"{self.name} bites {target.name}. {target.name}'s health is now {target.health}.")
-
-        if target.health <= 0:
-            Animal.alive.remove(target)
-            print(f"{target.name} has died.")
