7b561db34c6855288da026cca5e41b8c29de0474
Gena <gena.anzin@ukr.net>
2025-08-20 15:23:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+class Animal:
+    alive = []
+    def __init__(self, name: str, hidden=False) -> None:
+        self.health = 100
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden == True:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+class Carnivore(Animal):
+
+    def bite(self, animal: Herbivore) -> None:
+        if isinstance(animal, Herbivore) and animal.hidden == False:
+            animal.health -= 50
+            if animal.health <= 0:
+                Animal.alive.remove(animal)
+                print(f"{animal.name} has died.")
+        else:
+            print(f"{self.name} cannot bite {animal.name}. Either they are not a herbivore or they are hidden.")
+
+
