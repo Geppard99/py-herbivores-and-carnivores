025909bf6b02267eac3483ddf39ad87876bf0958
Gena <gena.anzin@ukr.net>
2025-08-21 12:44:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,31 +1,27 @@
 class Animal:
     alive = []
 
-    def __init__(
-            self,
-            name,
-            health=100
-    ):
+    def __init__(self,name: str,health: int = 100) -> None:
         self.health = health
         self.name = name
-        self.__hidden = False
+        self.__hidden: bool = False
 
         if self.health > 0:
             Animal.alive.append(self)
 
-    def get_hidden(self):
+    def get_hidden(self) -> bool:
         return self.__hidden
 
-    def set_hidden(self, value):
+    def set_hidden(self, value: bool) -> None:
         self.__hidden = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{{'Name': {self.name}, 'Health': {self.health}, 'Hidden': {self.get_hidden()}}}"
 
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.set_hidden(not self.get_hidden())
 
 
@@ -33,8 +29,8 @@ class Carnivore(Animal):
 
     def bite(
             self,
-            target
-    ):
+            target: Animal
+    ) -> None:
         if not isinstance(target, Herbivore) or target.get_hidden():
             return
 
